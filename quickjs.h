aaba1d5326d967a7f12d9ee86c93ce70ea93b0d6
Ivan Komissarov <abbapoh@gmail.com>
2025-01-06 20:25:14 +0300

    Add JS_IsSimpleValue

--- quickjs.h
+++ quickjs.h
@@ -743,6 +743,7 @@ JS_EXTERN bool JS_SetConstructorBit(JSContext *ctx, JSValue func_obj, bool val);
 
 JS_EXTERN bool JS_IsRegExp(JSValue val);
 JS_EXTERN bool JS_IsMap(JSValue val);
+JS_EXTERN int JS_IsSimpleValue(JSContext* ctx, JSValue v);
 
 JS_EXTERN JSValue JS_NewArray(JSContext *ctx);
 // takes ownership of the values
